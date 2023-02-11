8dc6f515d0294fc6be450da0f4940f5f9b4b2879
ahmedothman <ahmed.n.othman01@gmail.com>
2023-02-11 18:21:52 +0200

    updated deployment for question 4

--- hw2/q4/deployment.py
+++ hw2/q4/deployment.py
@@ -1,13 +1,14 @@
 from prefect.deployments import Deployment  
 from prefect_github.repository import GitHubRepository
 
+
 github_repository_block = GitHubRepository.load("gh-block")
+github_repository_block.get_directory()
 
-
+from etl_web_to_gcs import parent_flow
 deployment = Deployment.build_from_flow(
-    flow=my_flow,
+    flow=parent_flow,
     name="q4",
-    storage=github_repository_block,
 
 )
 
