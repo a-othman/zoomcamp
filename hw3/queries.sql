090afa80da85b35f9e48a1a9bdde67bfb6d292fa
ahmedothman <ahmed.n.othman01@gmail.com>
2023-02-13 18:52:29 +0200

    hw3 include q5 query

--- hw3/queries.sql
+++ hw3/queries.sql
@@ -9,3 +9,9 @@ SELECT  COUNT(DISTINCT Affiliated_base_number) FROM `zoomcamp-375102.bq.bq_rides
 SELECT  COUNT(*) 
 FROM `zoomcamp-375102.bq.bq_rides` r
 where r.PUlocationID is null and r.DOlocationID is null
+
+
+-- q5
+select distinct r.Affiliated_base_number
+from `zoomcamp-375102.partitioned.partitioned` r
+where DATE(r.pickup_datetime) >= '2019-03-01' and DATE(r.pickup_datetime) <= '2019-03-31'
