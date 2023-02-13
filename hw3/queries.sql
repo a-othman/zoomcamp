2e9fa1a2b3f16bf5b7892293e20d95252d5a4c7b
ahmedothman <ahmed.n.othman01@gmail.com>
2023-02-13 19:08:56 +0200

    updated q5

--- hw3/queries.sql
+++ hw3/queries.sql
@@ -12,6 +12,11 @@ where r.PUlocationID is null and r.DOlocationID is null
 
 
 -- q5
+CREATE OR REPLACE TABLE `zoomcamp-375102.bq.bq_partitioned`
+PARTITION BY
+  DATE(pickup_datetime) AS
+SELECT * FROM `zoomcamp-375102.bq.bq_rides`;
+
 select distinct r.Affiliated_base_number
-from `zoomcamp-375102.partitioned.partitioned` r
+from `zoomcamp-375102.bq.bq_partitioned` r
 where DATE(r.pickup_datetime) >= '2019-03-01' and DATE(r.pickup_datetime) <= '2019-03-31'
